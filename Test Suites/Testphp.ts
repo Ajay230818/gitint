fdbf61569d15064507f492aadd5dff203f951ac0
Ajay230818 <parthivajay.vt@gmail.com>
2022-05-12 12:12:42 +0530

    Created Test Suite

---
+++ Test Suites/Testphp.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testphp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1eca9ea2-d01a-4042-94f4-b16dc9f40239</testSuiteGuid>
+   <testCaseLink>
+      <guid>cfad886c-8adf-4267-a1fe-264c8701f621</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/phpcart</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
